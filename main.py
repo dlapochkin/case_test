da1354b8ee075a9e60f02e89716bd2cfb568e2c5
dlapochkin <d.lapochkin@g.nsu.ru>
2021-02-15 13:39:27 +0500

    pre-final

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+from textblob import TextBlob
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+text = input()
+syllables = 0
+sentence = 0
+fre = 0
 
+for b in ['.', '!', '?']:
+    if text.count(b) > 0:
+        sentence += text.count(b)
+print('Предложений:', sentence)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print('Слов:', text.count(' ') + 1)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if ord(text[0]) < 122:
+    syllables = sum(1 for x in text.lower() if x in 'aeiou')
+else:
+    syllables = sum(1 for x in text.lower() if x in 'уеоаыяиюэ')
+print('Слогов:', syllables)
+print('Средняя длина предложения в словах:', (text.count(' ') + 1) / sentence)
+print('Средняя длина слова в слогах:', syllables / (text.count(' ') + 1))
+if ord(text[0]) < 122:
+    fre = 206.835 - (1.3 * asl) - (60.1 * asw)
+else:
+    fre = 206.835 - (1.015 * asl) - (84.6 * asw)
+print('Индекс удобочитаемости Флеша:', fre)
+blob = TextBlob(text)
+if blob.detect_language() == 'ru':
+    blob = blob.translate(to="en")
+if blob.sentiment.polarity > 0.33:
+    tonality = 'Положительная'
+elif blob.sentiment.polarity < -0.33:
+    tonality = 'Отрицательная'
+else:
+    tonality = 'Нейтральная'
+print('Тональность текста:', tonality)
+obj = format(1 - blob.sentiment.subjectivity, '.1%')
+print('Объективность:', obj)
