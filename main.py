61581b8642556883eb00a15db8bd71ef6d34bcbb
dlapochkin <d.lapochkin@g.nsu.ru>
2021-02-15 14:07:36 +0500

    pre-final

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ fre = 0
 for b in ['.', '!', '?']:
     if text.count(b) > 0:
         sentence += text.count(b)
-print('Предложений:', sentence)
-
-print('Слов:', text.count(' ') + 1)
 
 if blob.detect_language() == 'ru':
     syllables = sum(1 for x in text.lower() if x in 'уеоаыяиюэ')
@@ -25,14 +22,27 @@ else:
     syllables = sum(1 for x in text.lower() if x in 'aeiouy')
 asl = (text.count(' ') + 1) / sentence
 asw = syllables / (text.count(' ') + 1)
-print('Слогов:', syllables)
-print('Средняя длина предложения в словах:', asl)
-print('Средняя длина слова в слогах:', asw)
 
 if blob.detect_language() == 'ru':
     fre = 206.835 - (1.3 * (asl)) - (60.1 * asw)
 else:
     fre = 206.835 - (1.015 * asl) - (84.6 * asw)
+
+if blob.detect_language() == 'ru':
+    blob = blob.translate(to="en")
+if blob.sentiment.polarity > 0.33:
+    tonality = 'положительный'
+elif blob.sentiment.polarity < -0.33:
+    tonality = 'отрицательный'
+else:
+    tonality = 'нейтральный'
+obj = format(1 - blob.sentiment.subjectivity, '.1%')
+
+print('Предложений:', sentence)
+print('Слов:', text.count(' ') + 1)
+print('Слогов:', syllables)
+print('Средняя длина предложения в словах:', asl)
+print('Средняя длина слова в слогах:', asw)
 print('Индекс удобочитаемости Флеша:', fre)
 
 if fre > 80:
@@ -44,14 +54,5 @@ elif fre > 25:
 else:
     print('Текст трудно читается (для выпускников ВУЗов).')
 
-if blob.detect_language() == 'ru':
-    blob = blob.translate(to="en")
-if blob.sentiment.polarity > 0.33:
-    tonality = 'Положительная'
-elif blob.sentiment.polarity < -0.33:
-    tonality = 'Отрицательная'
-else:
-    tonality = 'Нейтральная'
 print('Тональность текста:', tonality)
-obj = format(1 - blob.sentiment.subjectivity, '.1%')
 print('Объективность:', obj)
